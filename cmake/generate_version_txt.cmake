7ed9093872a890922f9d7ecf00a4dfdab2693778
Jevin Sweval <jevinsweval@gmail.com>
2022-11-05 18:27:01 -0400

    musl begins to build

--- cmake/generate_version_txt.cmake
+++ cmake/generate_version_txt.cmake
@@ -4,12 +4,24 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
     COMMAND_ERROR_IS_FATAL ANY
 )
-execute_process(
-    COMMAND git -C ${picolibc_SOURCE_DIR} rev-parse HEAD
-    OUTPUT_VARIABLE picolibc_COMMIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    COMMAND_ERROR_IS_FATAL ANY
-)
+
+if (${LLVM_TOOLCHAIN_LIBC} STREQUAL "picolibc")
+    execute_process(
+        COMMAND git -C ${picolibc_SOURCE_DIR} rev-parse HEAD
+        OUTPUT_VARIABLE picolibc_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
+
+if (${LLVM_TOOLCHAIN_LIBC} STREQUAL "musl")
+    execute_process(
+        COMMAND git -C ${musl_SOURCE_DIR} rev-parse HEAD
+        OUTPUT_VARIABLE musl_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+endif()
 
 configure_file(
     ${CMAKE_CURRENT_LIST_DIR}/VERSION.txt.in
